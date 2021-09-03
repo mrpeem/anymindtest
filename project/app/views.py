72bb5056bd3a89863ae0e2073ca2fadbdfe14684
Peem <mrpeem@gmail.com>
2021-09-03 06:35:51 -0700

    remove limit function from views

--- project/app/views.py
+++ project/app/views.py
@@ -9,21 +9,6 @@ from rest_framework.response import Response
 from app.controllers.user_tweets import get_tweets_from_username
 from app.controllers.hashtags_search import get_tweets_from_hashtags
 
-
-def getLimit(limit):
-  defaultLimit = 30
-  if limit is None:
-    limit = defaultLimit
-  else:
-    try:
-      limit = int(limit)
-
-      # limit must be between 5 and 100
-      if limit < 5 or limit > 100:
-        limit = defaultLimit
-    except ValueError:
-      limit = defaultLimit
-  return limit
     
 class hashtagsList(APIView):
     def get(self, request, hashtag=None):
